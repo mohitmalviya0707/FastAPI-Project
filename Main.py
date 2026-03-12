990f05bd0286d24e1d69aeb6291f3d3bae3794ea
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-03-12 08:31:21 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,8 +9,7 @@ app = FastAPI()
 class Patient(BaseModel):
 
     id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
-    name: Annotated[str, Field(..., description='Name of the patient')]
-    city: Annotated[str, Field(..., description='City where the patient is living')]
+
 
 
 
