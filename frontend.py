169895c064b1938bb84e7ce37f08cd284149a2a8
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-03-14 20:44:42 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,2 +1,34 @@
 import streamlit as st
 import requests
+   "Occupation",
+    ['retired', 'freelancer', 'student', 'government_job', 'business_owner', 'unemployed', 'private_job']
+)
+
+if st.button("Predict Premium Category"):
+    input_data = {
+        "age": age,
+        "weight": weight,
+        "height": height,
+        "income_lpa": income_lpa,
+        "smoker": smoker,
+        "city": city,
+        "occupation": occupation
+    }
+
+    try:
+        response = requests.post(API_URL, json=input_data)
+        result = response.json()
+
+        if response.status_code == 200 and "response" in result:
+            prediction = result["response"]
+            st.success(f"Predicted Insurance Premium Category: **{prediction['predicted_category']}**")
+            st.write("🔍 Confidence:", prediction["confidence"])
+            st.write("📊 Class Probabilities:")
+            st.json(prediction["class_probabilities"])
+
+        else:
+            st.error(f"API Error: {response.status_code}")
+            st.write(result)
+
+    except requests.exceptions.ConnectionError:
+        st.error("❌ Could not connect to the FastAPI server. Make sure it's running.")
