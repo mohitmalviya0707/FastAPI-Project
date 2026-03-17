e6f36a4e4cf19b0421dfc3211726cfe4f51e9db4
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-03-17 10:48:38 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
 from typing import Annotated, Literal, Optional
 import json
-
+   
 app = FastAPI()    
 
 class Patient(BaseModel):
