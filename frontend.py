d9544febe2264d2786a74cf908823c8743c4488b
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-03-17 10:49:43 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import requests                         ffffffffffffffff
+import requests                         ffffffffffffff
 
 API_URL = "http://34.226.152.222:8000/predict" 
 
