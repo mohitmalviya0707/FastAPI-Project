0b649b3eb2cc57c64f34caaf4a0715324af4dcee
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-04-23 12:50:49 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -7,7 +7,7 @@ st.title("Insurance Premium Category Predictor")
 st.markdown("Enter your details below:"). 
 
 # Input fields
-age = st.number_input("Age", min_value=1, max_value=119, value=30)
+age = st.number_input("Age", min_value=1,  max_value=119, value=30)
 weight = st.number_input("Weight (kg)", min_value=1.0, value=65.0)
 height = st.number_input("Height (m)", min_value=0.5, max_value=2.5, value=1.7)
 income_lpa = st.number_input("Annual Income (LPA)", min_value=0.1, value=10.0)
